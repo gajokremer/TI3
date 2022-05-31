42cef20eb37973de4e4c550bfd6cbff8a5d1f1e3
Gabriel Kremer <gajokremer@gmail.com>
2022-05-31 10:48:56 -0500

    Created basic GUI outline.

--- src/ui/ControllerGUI.java
+++ src/ui/ControllerGUI.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -18,11 +20,18 @@ public class ControllerGUI {
     private Pane mainPane;
 
     @FXML
+    private ImageView ivLogo;
+
+    @FXML
     void start(ActionEvent event) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
         fxmlLoader.setController(this);
         Parent menu = fxmlLoader.load();
         mainPane.getChildren().setAll(menu);
+
+//        Image logo = new Image("PlaneLogo.heic");
+        Image logo = new Image("https://www.seekpng.com/png/full/105-1051251_cartoon-airplane-png.png");
+        ivLogo.setImage(logo);
     }
 }
